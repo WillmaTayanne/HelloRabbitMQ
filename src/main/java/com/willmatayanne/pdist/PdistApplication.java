f7aeadac1da59a9ab6f1e88a8653d461ebc0fccb
WillmaTayanne <willmatayanne@gmail.com>
2022-04-08 22:49:51 -0300

    Atividade subindo

---
+++ src/main/java/com/willmatayanne/pdist/PdistApplication.java
@@ -0,0 +1,13 @@
+package com.willmatayanne.pdist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willmatayanne/pdist/PdistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willmatayanne.pdist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
